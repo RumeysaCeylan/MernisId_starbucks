f2970e08d3bb20a6964bb2017f8953b04c6824ee
RumeysaCeylan <59581456+RumeysaCeylan@users.noreply.github.com>
2021-05-15 13:40:01 +0300

    3

--- Starbucks_Mernis/src/Adapter/MernisServiceAdapter.java
+++ Starbucks_Mernis/src/Adapter/MernisServiceAdapter.java
@@ -7,23 +7,29 @@ import Entities.Customer;
 import tr.gov.nvi.tckimlik.WS.KPSPublicSoapProxy;
 
 public class MernisServiceAdapter implements ICustomerCheckService {
+	@SuppressWarnings("deprecation")
 	@Override
 	public boolean CheckIfRealPerson(Customer customer) {
 		
 		
 		KPSPublicSoapProxy proxy = new KPSPublicSoapProxy();
-		boolean result = false;
+		boolean result=false;
 		
-		try 
-		{
-			result = proxy.TCKimlikNoDogrula(Long.parseLong(customer.getNationalityId()),customer.getFirstName().toUpperCase(),customer.getLastName().toUpperCase(), customer.getDateOfBirth().getYear());		
-		} 
-		catch (Exception e) 
-		{
-			System.out.println("Not a valid person");
-		}
 		
-		return result;
+try {
+	result= proxy.TCKimlikNoDogrula(Long.parseLong(customer.getNationalityId()),
+			customer.getFirstName().toUpperCase(),customer.getLastName().toUpperCase(), 
+			customer.getDateOfBirth().getYear());
+} catch (NumberFormatException e) {
+	// TODO Auto-generated catch block
+	e.printStackTrace();
+} catch (RemoteException e) {
+	// TODO Auto-generated catch block
+	e.printStackTrace();
+}
+return result;		
+		
+	
 				
 		
 	}
--- Starbucks_Mernis/src/Main.java
+++ Starbucks_Mernis/src/Main.java
@@ -12,9 +12,9 @@ public class Main {
 	public static void main(String[] args) {
 		BaseCustomerManager baseCustomerManager = new StarbucksCustomerManager(new MernisServiceAdapter());
 		Customer customer =new Customer();
-		customer.setDateOfBirth(new GregorianCalendar(1999, 25 , 10).getTime());
-		customer.setFirstName("abcd");
-		customer.setLastName("efgh");
+		customer.setDateOfBirth(new GregorianCalendar(1999,10,25).getTime());
+		customer.setFirstName("aaa");
+		customer.setLastName("bbb");
 		customer.setNationalityId("123456789");
 		baseCustomerManager.Save(customer);
 		
